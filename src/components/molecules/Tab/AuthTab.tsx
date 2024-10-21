16ab73fa6fb4782b49b4b2a3e1e510d49799cdfb
datnt2002 <trongdat211002@gmail.com>
2024-10-21 17:56:54 +0700

    login UI

--- src/components/molecules/Tab/AuthTab.tsx
+++ src/components/molecules/Tab/AuthTab.tsx
@@ -1,22 +1,13 @@
 import { Tabs, TabsProps } from "antd";
 import RegisterForm from "@/components/organisms/Form/RegisterForm";
+import LoginForm from "@/components/organisms/Form/LoginForm";
 
 const items: TabsProps["items"] = [
-  //   {
-  //     key: "1",
-  //     label: "Log in",
-  //     children: (
-  //       <>
-  //         <LoginForm />
-  //         <AuthFormFooter
-  //           href={"/"}
-  //           hrefLabel="sign up"
-  //           message="Don’t have an account ?"
-  //           title="Or Log in With"
-  //         />
-  //       </>
-  //     ),
-  //   },
+  {
+    key: "1",
+    label: "Log in",
+    children: <LoginForm />,
+  },
   {
     key: "2",
     label: "Create Account",
---
+++ src/components/organisms/Form/LoginForm.tsx
@@ -0,0 +1,90 @@
+import type { FormProps } from "antd";
+import { Button, Checkbox, Form, Input } from "antd";
+
+type FieldType = {
+  principle: string;
+  password: string;
+  remember?: string;
+};
+
+const LoginForm = () => {
+  const [form] = Form.useForm();
+  const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+    // dispatch(loginThunk({ email: values.email, password: values.password }));
+    // dispatch(
+    //   setModalState({
+    //     key: "authModal",
+    //     isOpen: false,
+    //   })
+    // );
+  };
+
+  return (
+    <>
+      <h2 className="mt-4 text-center mb-5">Log in to Tech Heim</h2>
+      <Form form={form} onFinish={onFinish}>
+        <Form.Item<FieldType>
+          name="principle"
+          rules={[
+            { required: true, message: "Please input your email or username!" },
+          ]}
+        >
+          <Input
+            size="large"
+            prefix={
+              <img
+                src="/assets/icons/email/email_icon.svg"
+                className="h-4 mr-2"
+              />
+            }
+            placeholder="Please input your email or username!"
+          />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password
+            iconRender={(visible) => {
+              return visible ? (
+                <img src="/assets/icons/security/eye_visible.svg" />
+              ) : (
+                <img src="/assets/icons/security/eye_unvisible_icon.svg" />
+              );
+            }}
+            size="large"
+            prefix={
+              <img
+                src="/assets/icons/security/password_icon.svg"
+                className="h-4 mr-2"
+              />
+            }
+            placeholder="Password"
+          />
+        </Form.Item>
+
+        <div className="flex justify-between h-6 mb-2">
+          <Form.Item<FieldType> name="remember" valuePropName="checked">
+            <Checkbox className="text-gray-717171">Keep me logged in</Checkbox>
+          </Form.Item>
+          {/* <Link className="text-primary" to="/">
+            Forgot password ?
+          </Link> */}
+        </div>
+        <Form.Item>
+          <Button
+            size="large"
+            className="w-full"
+            type="primary"
+            htmlType="submit"
+          >
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
+
+export default LoginForm;
